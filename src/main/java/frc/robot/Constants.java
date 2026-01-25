76f28d76dd839ce8d488bac4caed1da08ae023b2
Cowpow0987 <evankempin1@outlook.com>
2026-01-24 21:29:08 -0600

    Added more motors

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -13,10 +13,16 @@ public class Constants {
 
     public class ClimberConstants {
 
-        /** CAN ID of outer chain motor. */
-        public static final int kOuterMotorCANID = 0;
+        /** CAN ID of the first outer chain motor. */
+        public static final int kOuterMotor1CANID = 0;
 
-        /** CAN ID of inner chain motor. */
-        public static final int kInnerMotorCANID = 0;
+        /** Can ID of the second outer chain motor. */
+        public static final int kOuterMotor2CANID = 0;
+
+        /** CAN ID of the first inner chain motor. */
+        public static final int kInnerMotor1CANID = 0;
+
+        /** CAN ID of the second inner chain motor. */
+        public static final int kInnerMotor2CANID = 0;
     }
 }
--- src/main/java/frc/robot/subsystems/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -5,10 +5,12 @@
 package frc.robot.subsystems;
 
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.controls.Follower;
 import com.ctre.phoenix6.controls.MotionMagicTorqueCurrentFOC;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.GravityTypeValue;
 import com.ctre.phoenix6.signals.InvertedValue;
+import com.ctre.phoenix6.signals.MotorAlignmentValue;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Calibrations.ClimberCalibrations;
@@ -16,75 +18,87 @@ import frc.robot.Constants.ClimberConstants;
 
 public class Climber extends SubsystemBase {
 
-  private final TalonFX m_motor1;
-  private final TalonFX m_motor2;
+  private final TalonFX m_outerMotor1;
+  private final TalonFX m_outerMotor2;
 
-  private final TalonFXConfiguration m_talonFXConfig1;
-  private final TalonFXConfiguration m_talonFXConfig2;
+  private final TalonFX m_innerMotor1;
+  private final TalonFX m_innerMotor2;
+
+  private final TalonFXConfiguration m_innerTalonFXConfig;
+  private final TalonFXConfiguration m_outerTalonFXConfig;
 
   private final MotionMagicTorqueCurrentFOC m_request1;
   private final MotionMagicTorqueCurrentFOC m_request2;
   /** Creates a new Climber. */
   public Climber() {
-    m_motor1 = new TalonFX(ClimberConstants.kOuterMotorCANID, "kachow");
-    m_motor2 = new TalonFX(ClimberConstants.kInnerMotorCANID, "kachow");
+    m_outerMotor1 = new TalonFX(ClimberConstants.kOuterMotor1CANID, "kachow");
+    m_outerMotor2 = new TalonFX(ClimberConstants.kOuterMotor2CANID, "kachow");
 
-    m_talonFXConfig1 = new TalonFXConfiguration();
-    m_talonFXConfig2 = new TalonFXConfiguration();
+    m_innerMotor1 = new TalonFX(ClimberConstants.kInnerMotor1CANID, "kachow");
+    m_innerMotor2 = new TalonFX(ClimberConstants.kInnerMotor2CANID, "kachow");
+
+    m_outerTalonFXConfig = new TalonFXConfiguration();
+    m_innerTalonFXConfig = new TalonFXConfiguration();
 
     m_request1 = new MotionMagicTorqueCurrentFOC(0);
     m_request2 = new MotionMagicTorqueCurrentFOC(0);
 
-    m_talonFXConfig1.MotorOutput.Inverted = InvertedValue.Clockwise_Positive;
-    m_talonFXConfig1.Slot0.GravityType = GravityTypeValue.Elevator_Static;
+    m_outerTalonFXConfig.MotorOutput.Inverted = InvertedValue.Clockwise_Positive;
+    m_outerTalonFXConfig.Slot0.GravityType = GravityTypeValue.Elevator_Static;
 
-    m_talonFXConfig1.Slot0.kG = ClimberCalibrations.kOuterkG;
-    m_talonFXConfig1.Slot0.kS = ClimberCalibrations.kOuterkS;
-    m_talonFXConfig1.Slot0.kP = ClimberCalibrations.kOuterkP;
-    m_talonFXConfig1.Slot0.kI = ClimberCalibrations.kOuterkI;
-    m_talonFXConfig1.Slot0.kD = ClimberCalibrations.kOuterkD;
+    m_outerTalonFXConfig.Slot0.kG = ClimberCalibrations.kOuterkG;
+    m_outerTalonFXConfig.Slot0.kS = ClimberCalibrations.kOuterkS;
+    m_outerTalonFXConfig.Slot0.kP = ClimberCalibrations.kOuterkP;
+    m_outerTalonFXConfig.Slot0.kI = ClimberCalibrations.kOuterkI;
+    m_outerTalonFXConfig.Slot0.kD = ClimberCalibrations.kOuterkD;
 
-    m_talonFXConfig1.MotionMagic.MotionMagicCruiseVelocity = ClimberCalibrations.kOuterCruiseVelocity;
-    m_talonFXConfig1.MotionMagic.MotionMagicAcceleration = ClimberCalibrations.kOuterAcceleration;
-    m_talonFXConfig1.MotionMagic.MotionMagicJerk = ClimberCalibrations.kOuterJerk;
+    m_outerTalonFXConfig.MotionMagic.MotionMagicCruiseVelocity = ClimberCalibrations.kOuterCruiseVelocity;
+    m_outerTalonFXConfig.MotionMagic.MotionMagicAcceleration = ClimberCalibrations.kOuterAcceleration;
+    m_outerTalonFXConfig.MotionMagic.MotionMagicJerk = ClimberCalibrations.kOuterJerk;
 
-    m_talonFXConfig1.SoftwareLimitSwitch.ForwardSoftLimitEnable = true;
-    m_talonFXConfig1.SoftwareLimitSwitch.ForwardSoftLimitThreshold = ClimberCalibrations.kOuterForwardSoftLimit;
+    m_outerTalonFXConfig.SoftwareLimitSwitch.ForwardSoftLimitEnable = true;
+    m_outerTalonFXConfig.SoftwareLimitSwitch.ForwardSoftLimitThreshold = ClimberCalibrations.kOuterForwardSoftLimit;
 
-    m_talonFXConfig1.SoftwareLimitSwitch.ReverseSoftLimitEnable = true;
-    m_talonFXConfig1.SoftwareLimitSwitch.ReverseSoftLimitThreshold = ClimberCalibrations.kOuterReverseSoftLimit;
+    m_outerTalonFXConfig.SoftwareLimitSwitch.ReverseSoftLimitEnable = true;
+    m_outerTalonFXConfig.SoftwareLimitSwitch.ReverseSoftLimitThreshold = ClimberCalibrations.kOuterReverseSoftLimit;
 
-    m_motor1.getConfigurator().apply(m_talonFXConfig1);
+    m_outerMotor1.getConfigurator().apply(m_outerTalonFXConfig);
+    m_outerMotor2.getConfigurator().apply(m_outerTalonFXConfig);
 
-    m_talonFXConfig2.MotorOutput.Inverted = InvertedValue.Clockwise_Positive;
-    m_talonFXConfig2.Slot0.GravityType = GravityTypeValue.Elevator_Static;
+    m_outerMotor2.setControl(new Follower(ClimberConstants.kOuterMotor1CANID, MotorAlignmentValue.Aligned));
 
-    m_talonFXConfig2.Slot0.kG = ClimberCalibrations.kInnerkG;
-    m_talonFXConfig2.Slot0.kS = ClimberCalibrations.kInnerkS;
-    m_talonFXConfig2.Slot0.kP = ClimberCalibrations.kInnerkP;
-    m_talonFXConfig2.Slot0.kI = ClimberCalibrations.kInnerkI;
-    m_talonFXConfig2.Slot0.kD = ClimberCalibrations.kInnerkD;
+    m_innerTalonFXConfig.MotorOutput.Inverted = InvertedValue.Clockwise_Positive;
+    m_innerTalonFXConfig.Slot0.GravityType = GravityTypeValue.Elevator_Static;
 
-    m_talonFXConfig2.MotionMagic.MotionMagicCruiseVelocity = ClimberCalibrations.kInnerCruiseVelocity;
-    m_talonFXConfig2.MotionMagic.MotionMagicAcceleration = ClimberCalibrations.kInnerAcceleration;
-    m_talonFXConfig2.MotionMagic.MotionMagicJerk = ClimberCalibrations.kInnerJerk;
+    m_innerTalonFXConfig.Slot0.kG = ClimberCalibrations.kInnerkG;
+    m_innerTalonFXConfig.Slot0.kS = ClimberCalibrations.kInnerkS;
+    m_innerTalonFXConfig.Slot0.kP = ClimberCalibrations.kInnerkP;
+    m_innerTalonFXConfig.Slot0.kI = ClimberCalibrations.kInnerkI;
+    m_innerTalonFXConfig.Slot0.kD = ClimberCalibrations.kInnerkD;
 
-    m_talonFXConfig2.SoftwareLimitSwitch.ForwardSoftLimitEnable = true;
-    m_talonFXConfig2.SoftwareLimitSwitch.ForwardSoftLimitThreshold = ClimberCalibrations.kInnerForwardSoftLimit;
+    m_innerTalonFXConfig.MotionMagic.MotionMagicCruiseVelocity = ClimberCalibrations.kInnerCruiseVelocity;
+    m_innerTalonFXConfig.MotionMagic.MotionMagicAcceleration = ClimberCalibrations.kInnerAcceleration;
+    m_innerTalonFXConfig.MotionMagic.MotionMagicJerk = ClimberCalibrations.kInnerJerk;
 
-    m_talonFXConfig2.SoftwareLimitSwitch.ReverseSoftLimitEnable = true;
-    m_talonFXConfig2.SoftwareLimitSwitch.ReverseSoftLimitThreshold = ClimberCalibrations.kInnerReverseSoftLimit;
+    m_innerTalonFXConfig.SoftwareLimitSwitch.ForwardSoftLimitEnable = true;
+    m_innerTalonFXConfig.SoftwareLimitSwitch.ForwardSoftLimitThreshold = ClimberCalibrations.kInnerForwardSoftLimit;
 
-    m_motor2.getConfigurator().apply(m_talonFXConfig2);
+    m_innerTalonFXConfig.SoftwareLimitSwitch.ReverseSoftLimitEnable = true;
+    m_innerTalonFXConfig.SoftwareLimitSwitch.ReverseSoftLimitThreshold = ClimberCalibrations.kInnerReverseSoftLimit;
+
+    m_innerMotor1.getConfigurator().apply(m_innerTalonFXConfig);
+    m_innerMotor2.getConfigurator().apply(m_innerTalonFXConfig);
+
+    m_innerMotor2.setControl(new Follower(ClimberConstants.kInnerMotor1CANID, MotorAlignmentValue.Aligned));
   }
 
   public void setOuterClimberSetpoint(double newSetpoint) {
-    m_motor1.setControl(m_request1
+    m_outerMotor1.setControl(m_request1
       .withPosition(newSetpoint));
   }
 
   public void setInnerClimberSetpoint(double newSetpoint) {
-    m_motor2.setControl(m_request2
+    m_innerMotor1.setControl(m_request2
       .withPosition(newSetpoint));
   }
 
