a2974195333e8802f7c16a18bec2a5c33ce2a498
Cowpow0987 <evankempin1@outlook.com>
2026-01-31 20:45:29 -0600

    Changed request type to use a PID on the RIO in order to be compatible with a potentiometer

--- src/main/java/frc/robot/subsystems/hood.java
+++ src/main/java/frc/robot/subsystems/hood.java
@@ -6,20 +6,25 @@ package frc.robot.subsystems;
 
 import com.ctre.phoenix6.configs.TalonFXSConfiguration;
 import com.ctre.phoenix6.controls.MotionMagicTorqueCurrentFOC;
+import com.ctre.phoenix6.controls.TorqueCurrentFOC;
 import com.ctre.phoenix6.hardware.TalonFXS;
 import com.ctre.phoenix6.signals.BrushedMotorWiringValue;
+import com.ctre.phoenix6.signals.ExternalFeedbackSensorSourceValue;
 import com.ctre.phoenix6.signals.InvertedValue;
 import com.ctre.phoenix6.signals.NeutralModeValue;
 
+import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class hood extends SubsystemBase {
-
+  
   private TalonFXS m_motor;
-
+  
   private TalonFXSConfiguration m_talonFXSConfig;
-
-  private MotionMagicTorqueCurrentFOC m_request;
+  
+  private TorqueCurrentFOC m_request;
+  private PIDController m_pid;
+  private double m_setpoint = 0;
 
   /** Creates a new hood. */
   public hood() {
@@ -28,21 +33,14 @@ public class hood extends SubsystemBase {
 
     m_talonFXSConfig = new TalonFXSConfiguration();
 
-    m_request = new MotionMagicTorqueCurrentFOC(0);
+    m_request = new TorqueCurrentFOC(0);
+
+    m_pid = new PIDController(0, 0, 0);
 
     m_talonFXSConfig.Commutation.BrushedMotorWiring = BrushedMotorWiringValue.Leads_A_and_B;
 
     m_talonFXSConfig.MotorOutput.Inverted = InvertedValue.Clockwise_Positive;
     m_talonFXSConfig.MotorOutput.NeutralMode = NeutralModeValue.Brake;
-
-    m_talonFXSConfig.Slot0.kS = 0;
-    m_talonFXSConfig.Slot0.kP = 0;
-    m_talonFXSConfig.Slot0.kI = 0;
-    m_talonFXSConfig.Slot0.kD = 0;
-
-    m_talonFXSConfig.MotionMagic.MotionMagicCruiseVelocity = 0;
-    m_talonFXSConfig.MotionMagic.MotionMagicAcceleration = 0;
-    m_talonFXSConfig.MotionMagic.MotionMagicJerk = 0;
 
     m_talonFXSConfig.CurrentLimits.StatorCurrentLimit = 40;
 
@@ -51,11 +49,16 @@ public class hood extends SubsystemBase {
   }
 
   public void updateSetpoint(double newSetpoint) {
-    m_motor.setControl(m_request.withPosition(newSetpoint));
+    m_setpoint = newSetpoint;
   }
 
   @Override
   public void periodic() {
+    
+    m_motor.setControl(
+      m_request.withOutput(
+        m_pid.calculate(m_motor.getAnalogVoltage().getValueAsDouble(), m_setpoint)));
+
     // This method will be called once per scheduler run
   }
 }
